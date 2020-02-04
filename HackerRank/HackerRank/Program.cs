cfca5263f46b28c6448e089dd98099221c1938bd
surajkumar78 <sk390693@gmail.com>
2020-02-04 16:29:11 +0530

    My First Commit

---
+++ HackerRank/HackerRank/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+    class Program
+    {
+        static int simpleArraySum(int[] ar)
+        {
+            var sum = 0;
+            foreach(var item in ar)
+            {
+                sum += item;
+            }
+            return sum;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            //*****First Program*****
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int arCount = Convert.ToInt32(Console.ReadLine());
+
+            int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp));
+            int result = simpleArraySum(ar);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
---
+++ MatrixDiagAddition/MatrixDiagAddition/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixDiagAddition
+{
+    class Program
+    {
+        public static int diagonalDifference(List<List<int>> arr)
+        {
+            for (var i = 0; i < arr
+        }
+
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<List<int>> arr = new List<List<int>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+            }
+
+            int result = Program.diagonalDifference(arr);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
